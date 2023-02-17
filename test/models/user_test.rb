2c0d3fbe84718e7ff6afe97e04bce87fac7ef8e6
YM202c2100 <Y.M202c2100@gmail.com>
2023-02-17 11:51:33 +0900

    リスト6.7~6.9：name属性が存在するかどうか確認するためのテスト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,13 @@ class UserTest < ActiveSupport::TestCase
   def setup()
     @user = User.new(name: "Example User", email: "user@example.com")
   end
+
   test "should valid" do
     assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
   end
 end
